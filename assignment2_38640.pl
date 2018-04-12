8d28477d8fbf7b10581a09458d4627e4466f2c88
TristanSaunders <tristansaundersfr@gmail.com>
2018-04-12 15:20:31 +0100

    got Astar and bfs to work, demo runs fine

--- assignment2_38640.pl
+++ assignment2_38640.pl
@@ -3,25 +3,53 @@ candidate_number(38640).
 solve_task(Task,Cost):-
   my_agent(Agent),
   query_world( agent_current_position, [Agent,P] ),
-  %solve_task_bt(Task,[c(0,P),P],0,R,Cost,_NewPos),!,  % prune choice point for efficiency
-  solve_task_A_star(Task,[c(0,P,0),P],0,R,Cost,_NewPos),!,
+  Task =.. [Command,Goal],
+  (Goal = o(X) -> solve_task_bfs(Task,[[c(0,0,P),P]],0,R,Cost,_NewPos) % if we need to find oracle or charge point
+  ;Goal = c(X) -> solve_task_bfs(Task,[[c(0,0,P),P]],0,R,Cost,_NewPos) % use bfs as goal pos is unknown
+  ;otherwise -> G0 is 0, % else use Astar search
+                map_distance(P,Goal,H0), % original estimate of cost to get to goal
+                F0 is G0+H0,
+                solve_task_Astar(Task,[[c(F0,G0,P),P]],0,R,Cost,_NewPos)
+  ),
+  !,
   reverse(R,[_Init|Path]),
   query_world( agent_do_moves, [Agent,Path] ).
 
+% A-star search for go(Goal)
 solve_task_Astar(Task,Agenda,Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
   achieved(Task,Agenda,RPath,Cost,NewPos).
-solve_task_Astar(Task,Agenda,D,RPath,Cost,NewPos) :-
-  Agenda = [[c(F,P,G)|RPath]|Paths],
-  setof([c(F1,P1,G1),RPath],
-    (search(P,P1,R,C),\+ memberchk(R,RPath),G1 is G+C,map_distance(P1,Task,H),F1 is G1+H),
+solve_task_Astar(Task,Agenda,D,RR,Cost,NewPos) :-
+  Agenda = [[c(F,G,P)|RPath]|Paths],
+  Task = go(Goal),
+  findall([c(F1,G1,P1),R|RPath], %1st arg of findall is template of return value (what the return value should look like)
+    (search(P,P1,R,C), % find all children of curr pos
+     \+ memberchk(R,RPath), % check that returned child isnt already in path
+     G1 is G+C, % G is cost to arrive to curr pos, update G with cost to move to child
+     map_distance(P1,Goal,H), % calc H that is heuristic (estimated cost) to get to goal
+     F1 is G1+H), % update F which is total estimated cost to get to goal
+    Children), % store result in Children
+  sort(Children,SChildren),
+  D1 is D+1,
+  append(SChildren,Paths,NewAgenda), sort(NewAgenda, SNewAgenda), % append and sort is equivalent to merge
+  %mergelists(SChildren,Paths,NewAgenda), % doesnt work... so using append+sort for now
+  solve_task_Astar(Task,SNewAgenda,D1,RR,Cost,NewPos).
+
+% Breadth-first search for o(Goal) and c(Goal) as don't know location of oracles and charging points
+solve_task_bfs(Task,Agenda,Depth,RPath,[cost(Cost),depth(Depth)],NewPos) :-
+  achieved(Task,Agenda,RPath,Cost,NewPos).
+solve_task_bfs(Task,Agenda,D,RR,Cost,NewPos) :-
+  Agenda = [[c(F,F,P)|RPath]|Paths], % dont know position of o and c bfs -> no heuristic/ estimated cost to goal
+  %Task = find(Goal),
+  findall([c(F1,F1,P1),R|RPath],
+    (search(P,P1,R,C),\+memberchk(R,RPath),F1 is F+C,\+member([c(_,_,P1)|_],Paths)),
     Children),
   %sort(Children,SChildren),
   D1 is D+1,
-  %append(Children,Paths,newAgenda),
-  mergelists(Children,Agenda,NewAgenda), %Can also just append and sort
-  solve_task_Astar(Task,NewAgenda,D1,RR,Cost,NewPos).
+  append(Paths, Children, NewAgenda), % not sure why we just append...
+  solve_task_bfs(Task,NewAgenda,D1,RR,Cost,NewPos).
 
-getChildren(F,P,G,RPath) :-
+
+getChildren(F,G,P,RPath) :-
   search(P,P1,R,C),
   \+ memberchk(R,RPath),
   G1 is G+C,
@@ -55,12 +83,12 @@ solve_task_bt(Task,Current,D,RR,Cost,NewPos) :-
   solve_task_bt(Task,[c(F1,P1),R|RPath],D1,RR,Cost,NewPos).  % backtrack search
 
 achieved(go(Exit),Current,RPath,Cost,NewPos) :-
-  Current = [c(Cost,NewPos)|RPath],
+  Current = [[c(Cost,_,NewPos)|RPath]|_],
   ( Exit=none -> true
   ; otherwise -> RPath = [Exit|_]
   ).
 achieved(find(O),Current,RPath,Cost,NewPos) :-
-  Current = [c(Cost,NewPos)|RPath],
+  Current = [[c(Cost,_,NewPos)|RPath]|_],
   ( O=none    -> true
   ; otherwise -> RPath = [Last|_],map_adjacent(Last,_,O)
   ).
