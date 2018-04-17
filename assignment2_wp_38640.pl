da280ecf1134da32e109db6aa4fde607b62933ff
smudgecat123 <smudgecat123@users.noreply.github.com>
2018-04-17 13:07:18 +0100

    part 2 complete

--- assignment2_wp_38640.pl
+++ assignment2_wp_38640.pl
@@ -8,7 +8,15 @@ find_identity(A):-
   ).
 
 find_identity_2(A):-
-  A='Not yet implemented'.
+  findall(Actor, actor(Actor), Actors), %get all actors
+  find_identity_2(A, Actors).
+find_identity_2(A, Actors) :-
+  agent_ask_oracle(oscar,o(1),link,L),
+  findall(Actor,(wp:actor_links(Actor,Links), member(L, Links), member(Actor, Actors)), FilteredActors),
+  writeln("Possible:" : FilteredActors),
+  (FilteredActors = [Answer|[]] -> A = Answer
+  ;otherwise -> find_identity_2(A, FilteredActors)
+  ).
 
 find_identity_o(A):-
   A='Not yet implemented'.
